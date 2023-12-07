0fead65f0a8a9b8c437d33d3f2fdeb94bd82c8cf
Diégo Mazenc <contact@diego-mazenc.com>
2023-12-07 15:17:09 +0100

    mise en fonction du filtre par marque

--- src/stores/stock.js
+++ src/stores/stock.js
@@ -4,8 +4,8 @@ import { defineStore } from 'pinia'
 export const useJoujouStore = defineStore('joujou', {
   state: () => ({
     joujouListe: joujouListe,
-    filteredList: joujouListe, 
     selectedMarque: 'all',
+    filteredList: joujouListe,
     triAlpha: false,
     triPrix: false
   }),
@@ -13,24 +13,10 @@ export const useJoujouStore = defineStore('joujou', {
   getters: {
     getJoujou: (state) => state.joujouListe,
     getSelectedMarque: (state) => state.selectedMarque,
-    getTriAlpha: (state) => state.triAlpha, 
+    getTriAlpha: (state) => state.triAlpha,
     getTriPrix: (state) => state.triPrix,
+    getfilteredList: (state) => state.filteredList,
 
-    getFilteredList: (state) => {
-      // associer le tableau original à une variable
-   let tab = this.joujouListe.slice(); 
-      // filtrer le tableau par marque
-      if (state.selectedMarque !== 'all') {
-        state.filteredList = filteredList.filter(item => item.la_marque === this.selectedMarque)
-      }
-
-      filteredList = this.tab; 
-      // organiser le trix par alphabetique ou par prix
-
-      // retourner le tableau filtré
-    
-      return this.tab; 
-    }
   },
 
   actions: {
@@ -43,11 +29,12 @@ export const useJoujouStore = defineStore('joujou', {
     },
     updateSelectedMarque(selectedMarque) {
       // Mettez à jour la valeur du sélecteur dans le state
-      console.log('old', this.selectedMarque)
-      console.log(selectedMarque)
       this.selectedMarque = selectedMarque
-      console.log('new', this.selectedMarque)
-      console.log(this.filteredList); 
+      this.filteredList = joujouListe
+      if (this.selectedMarque !== 'all'){
+        this.filteredList = this.filteredList.filter(item => item.la_marque === this.selectedMarque)
+      }
+
     },
 
     updateTriAlpha() {
@@ -58,14 +45,11 @@ export const useJoujouStore = defineStore('joujou', {
     },
 
     updateTriPrix() {
-      
       this.triAlpha = false
       this.triPrix = true
       // console.log('TRIPRI marche', this.triPrix, "alpha", this.triAlpha)
     },
 
-    debug () {
-      console.log(this.getFilteredList); 
-    }
+  
   }
 })
