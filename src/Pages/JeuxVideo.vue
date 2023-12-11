26f081588d586fd431385da1f77fa8852b438a6b
Diégo Mazenc <contact@diego-mazenc.com>
2023-12-11 16:31:35 +0100

    repush

--- src/Pages/JeuxVideo.vue
+++ src/Pages/JeuxVideo.vue
@@ -2,14 +2,14 @@
 import Display from '../components/compoJV/Display.vue'
 import Counter from '../components/compoJV/Counter.vue';
 import FilterItem from '../components/compoJV/FilterItem.vue';
+
 export default{
     name: "JeuxVideoView",
     components:{
-
         Display,
         Counter,
         FilterItem
-        
+
     }
 }
 
