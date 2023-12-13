d7b14a58c8c38fca2057ef5e0582e9bbb71b7d7b
Diégo Mazenc <contact@diego-mazenc.com>
2023-12-13 15:02:33 +0100

    mise en page de joujou

--- src/Pages/JouJouView.vue
+++ src/Pages/JouJouView.vue
@@ -9,21 +9,88 @@ export default {
     Display,
     FilterItem
   }, 
+  data() {
+  return {
+    isFilterItemFixed: false,
+  };
+},
   emits:['ajoutPanierJoujou'], 
   methods:{
     ajoutPanierJoujou(item){
       this.$emit('ajoutPanierJoujou', item)
-    }
-  }
+    },
+    handleScroll() {
+    const filterItem = this.$el; // Obtenez la référence de l'élément FilterItem
+    const filterItemOffsetTop = filterItem.offsetTop;
+
+    // Ajoutez votre propre valeur pour déterminer quand l'élément doit être fixé
+    this.isFilterItemFixed = window.scrollY > filterItemOffsetTop;
+  },
+    },
+
+    mounted() {
+  window.addEventListener('scroll', this.handleScroll);
+},
+
+beforeUnmount() {
+  window.removeEventListener('scroll', this.handleScroll);
+},
 }
 </script>
 
 <template>
-  <div>
-    <FormAdd />
-    <FilterItem />
-    <Display @ajoutPanierJoujou="ajoutPanierJoujou"/>
+   <div class="mainJV">
+    <div class="asideBar" :class="{ 'filter-item-fixed': isFilterItemFixed }">
+      <FilterItem />
+    </div>
+    <Display
+      class="mainDisplay"
+      :class="{ marginMainDisplay: isFilterItemFixed }"
+      @ajoutPanierJoujou="ajoutPanierJoujou"
+    />
   </div>
 </template>
 
-<style></style>
+<style>
+
+.mainJV {
+  display: flex;
+}
+
+.mainDisplay {
+  margin: 15px;
+}
+
+.marginMainDisplay {
+  margin-left: 350px;
+}
+
+.asideBar {
+  overflow-y: scroll;
+  min-width: 330px;
+  height: 100vh;
+  padding: 15px;
+  border-right: 2px rgb(223, 223, 223) solid;
+  background-color: #fafafa;
+
+  scrollbar-width: thin; /* Pour Firefox */
+  scrollbar-color: #cccccc #f0f0f0;
+}
+
+.asideBar::-webkit-scrollbar {
+  width: 5px; /* Largeur de la barre de défilement */
+}
+
+.asideBar::-webkit-scrollbar-thumb {
+  background-color: #d1d1d1; /* Couleur de la barre de défilement */
+}
+
+.asideBar::-webkit-scrollbar-track {
+  background-color: #ffffff; /* Couleur du fond de défilement */
+}
+
+.filter-item-fixed {
+  position: fixed;
+  top: 0;
+}
+</style>
--- src/components/compoJoujou/FilterItem.vue
+++ src/components/compoJoujou/FilterItem.vue
@@ -2,6 +2,7 @@
 import { mapState } from 'pinia'
 import { mapActions } from 'pinia'
 import { useJoujouStore } from '../../stores/stock.js'
+import PanierArticles from '../../components/PanierArticles.vue'
 
 export default {
   name: 'FilterItem',
@@ -9,60 +10,119 @@ export default {
     return {
       la_marque: null,
       tabMarque: [],
-      selectedMarque: "all", 
-      triAlpha : false, 
-      triPrix : false
+      selectedMarque: 'all',
+      triAlpha: false,
+      triPrix: false
     }
   },
+
+  components: {
+    PanierArticles
+  },
   methods: {
-    ...mapActions(useJoujouStore, {updateSelectedMarqueAction : "updateSelectedMarque" }),
+    ...mapActions(useJoujouStore, { updateSelectedMarqueAction: 'updateSelectedMarque' }),
     exportSelectedMarque() {
       // Appelez l'action du store pour mettre à jour la valeur dans le store
-      this.updateSelectedMarqueAction(this.selectedMarque);
-    }, 
+      this.updateSelectedMarqueAction(this.selectedMarque)
+    },
 
-    ...mapActions(useJoujouStore, {updateTriAlphaAction : "updateTriAlpha" }),
+    ...mapActions(useJoujouStore, { updateTriAlphaAction: 'updateTriAlpha' }),
     exportTrieAlpha() {
       // Appelez l'action du store pour mettre à jour la valeur dans le store
-      this.updateTriAlphaAction(this.triAlpha);
-    }, 
+      this.updateTriAlphaAction(this.triAlpha)
+    },
 
-    ...mapActions(useJoujouStore, {updateTriPrixAction : "updateTriPrix" }),
+    ...mapActions(useJoujouStore, { updateTriPrixAction: 'updateTriPrix' }),
     exportTriePrix() {
       // Appelez l'action du store pour mettre à jour la valeur dans le store
-      this.updateTriPrixAction(this.triPrix);
-    }, 
-
+      this.updateTriPrixAction(this.triPrix)
+    }
   },
   computed: {
     ...mapState(useJoujouStore, ['joujouListe']),
     uniqueMarques() {
-    
-      const uniqueMarquesSet = new Set();
+      const uniqueMarquesSet = new Set()
 
-     
-      this.joujouListe.forEach(joujou => {
-        uniqueMarquesSet.add(joujou.la_marque);
-      });
+      this.joujouListe.forEach((joujou) => {
+        uniqueMarquesSet.add(joujou.la_marque)
+      })
 
-  
-      return Array.from(uniqueMarquesSet);
-    },
-
-  },
-
+      return Array.from(uniqueMarquesSet)
+    }
+  }
 }
 </script>
 
 <template>
   <div>
-    <button @click="exportTrieAlpha">Alphabétique</button>
-    <button @click="exportTriePrix">Prix</button>
-    <select  class="card" style="width: 18rem" v-model="selectedMarque" @change="exportSelectedMarque">
-      <option value="all">All</option>
-      <option v-for="(item, index) in uniqueMarques" :key="index" :value="item">{{ item }}</option>
-    </select>
+    <div class="options-container">
+      <button @click="exportTrieAlpha">Alphabétique</button>
+      <button @click="exportTriePrix">Prix</button>
+      <div class="custom-select">
+        <select
+          class="card selectOption"
+          style="width: 18rem"
+          v-model="selectedMarque"
+          @change="exportSelectedMarque"
+        >
+          <option value="all">All</option>
+          <option v-for="(item, index) in uniqueMarques" :key="index" :value="item">
+            {{ item }}
+          </option>
+        </select>
+      </div>
+    </div>
+
+    <PanierArticles />
   </div>
 </template>
 
-<style></style>
+<style>
+.options-container {
+  display: flex;
+  flex-direction: column;
+}
+
+button,
+select {
+  margin-bottom: 10px;
+}
+
+.custom-select {
+  position: relative;
+  width: 18rem;
+}
+
+.custom-select select {
+  width: 100%;
+  padding: 10px;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  appearance: none; /* Style par défaut de la flèche de la liste déroulante */
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  cursor: pointer;
+}
+
+.custom-select select:focus {
+  outline: none; /* Supprime la bordure focus par défaut sur certains navigateurs */
+}
+
+.custom-select::after {
+  content: '\25BC'; /* Flèche vers le bas */
+  position: absolute;
+  top: 50%;
+  right: 15px;
+  transform: translateY(-50%);
+  pointer-events: none;
+}
+
+.selectOption {
+  width: 100%;
+  border: none;
+  border-top: 1px gray solid;
+  border-bottom: 1px gray solid;
+  border-radius: 0;
+}
+</style>
