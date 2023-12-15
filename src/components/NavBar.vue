d7097ab54cc273612b17d86f216c72d2d079a065
Lucas Berthet <lu.berthet@gmail.com>
2023-12-15 09:50:05 +0100

    compo manga

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,82 +1,87 @@
-<script>
+<script setup>
 import { RouterLink } from 'vue-router'
-export default {
-    name:'NavBar',
-    components:{
-      RouterLink
-    }, 
-    props:{
-      panier:{
-        type:Array,
-        requested : false, 
-        default: function (){
-          return null
-        }
-      }
+
+const props = defineProps({
+  panier: {
+    type: Array,
+    requested: false,
+    default: function () {
+      return null
     }
-}
+  }
+})
+
 </script>
 
-
 <template>
-    <nav class="navbar navbar-expand-lg color-nav" data-bs-theme="dark">
-  <div class="container-fluid">
-    <img src="../assets/img/logoTurboJoujou.png" alt="TurboJoujou Logo" class="logoNav">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
-        <li class="nav-item ms-5">
+  <nav class="navbar navbar-expand-lg color-nav" data-bs-theme="dark">
+    <div class="container-fluid">
+      <img src="../assets/img/logoTurboJoujou.png" alt="TurboJoujou Logo" class="logoNav" />
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item ms-5">
             <RouterLink to="/" class="styleLinkNav">Home</RouterLink>
-        </li>
-        <li class="nav-item ms-5">
+          </li>
+          <li class="nav-item ms-5">
             <RouterLink to="/mangaspage" class="styleLinkNav">Mangas</RouterLink>
-        </li>
-        <li class="nav-item ms-5">
+          </li>
+          <li class="nav-item ms-5">
             <RouterLink to="/jeuxvideo" class="styleLinkNav">Jeux vidéos</RouterLink>
-        </li>
-        <!-- <li class="nav-item">
+          </li>
+          <!-- <li class="nav-item">
             <RouterLink to="/admin">Admin</RouterLink>
         </li> -->
         </ul>
         <div class="cart">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M17 18c-1.11 0-2 .89-2 2a2 2 0 0 0 2 2a2 2 0 0 0 2-2a2 2 0 0 0-2-2M1 2v2h2l3.6 7.59l-1.36 2.45c-.15.28-.24.61-.24.96a2 2 0 0 0 2 2h12v-2H7.42a.25.25 0 0 1-.25-.25c0-.05.01-.09.03-.12L8.1 13h7.45c.75 0 1.41-.42 1.75-1.03l3.58-6.47c.07-.16.12-.33.12-.5a1 1 0 0 0-1-1H5.21l-.94-2M7 18c-1.11 0-2 .89-2 2a2 2 0 0 0 2 2a2 2 0 0 0 2-2a2 2 0 0 0-2-2"/></svg>
-        <p>{{ panier.length }}</p>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <path
+              fill="currentColor"
+              d="M17 18c-1.11 0-2 .89-2 2a2 2 0 0 0 2 2a2 2 0 0 0 2-2a2 2 0 0 0-2-2M1 2v2h2l3.6 7.59l-1.36 2.45c-.15.28-.24.61-.24.96a2 2 0 0 0 2 2h12v-2H7.42a.25.25 0 0 1-.25-.25c0-.05.01-.09.03-.12L8.1 13h7.45c.75 0 1.41-.42 1.75-1.03l3.58-6.47c.07-.16.12-.33.12-.5a1 1 0 0 0-1-1H5.21l-.94-2M7 18c-1.11 0-2 .89-2 2a2 2 0 0 0 2 2a2 2 0 0 0 2-2a2 2 0 0 0-2-2"
+            />
+          </svg>
+          <p>{{ panier.length }}</p>
+        </div>
       </div>
     </div>
-  
-  </div>
-</nav>
+  </nav>
 </template>
 
 <style>
-
-.logoNav{
+.logoNav {
   height: 50px;
   position: relative;
   margin-left: 20px;
 }
-.cart{
+.cart {
   display: flex;
   flex-direction: row;
   justify-items: center;
   align-content: center;
-  gap: 1em; 
+  gap: 1em;
 }
 
-.color-nav{
+.color-nav {
   background: linear-gradient(#393bb1, #323486);
 }
 
-.styleLinkNav{
+.styleLinkNav {
   color: white;
   text-decoration: none;
   padding: 22px;
 }
 
-.styleLinkNav:hover{
+.styleLinkNav:hover {
   color: rgb(203, 215, 226);
 }
-
 </style>
--- src/components/compoManga/FilterItem.vue
+++ src/components/compoManga/FilterItem.vue
@@ -1,65 +1,44 @@
-<script>
-import { mapState } from 'pinia'
-import { mapActions } from 'pinia'
+<script setup>
+
 import { useMangaStore } from '../../stores/stock.js'
+import { computed } from 'vue';
 import PanierArticles from '../../components/PanierArticles.vue'
 
 
-export default {
-  name: 'FilterItem',
-  data() {
-    return {
-      la_marque: null,
-      tabMarque: [],
-      selectedMarque: 'all',
-      selectedTitle: 'all',
-      triAlpha: false,
-      triPrix: false,
-      mangaTitre: null, 
-      tomeInfo: null, 
-    }
-  },
-  components:{
-    PanierArticles
-  },
-  methods: {
-    ...mapActions(useMangaStore, { updateSelectedMarqueAction: 'updateSelectedMarque' }),
-    exportSelectedMarque() {
-      // Appelez l'action du store pour mettre à jour la valeur dans le store
-      this.updateSelectedMarqueAction(this.selectedMarque)
-    },
+const la_marque = null;
+const tabMarque = [];
+const selectedMarque = 'all'
+const selectedTitle = 'all'
+const triAlpha = false
+const triPrix = false
+const mangaTitre = null
+const tomeInfo = null
 
-    ...mapActions(useMangaStore, { updateTriAlphaAction: 'updateTriAlpha' }),
-    exportTrieAlpha() {
-      // Appelez l'action du store pour mettre à jour la valeur dans le store
-      this.updateTriAlphaAction(this.triAlpha)
-    },
 
-    ...mapActions(useMangaStore, { updateTriPrixAction: 'updateTriPrix' }),
-    exportTriePrix() {
-      // Appelez l'action du store pour mettre à jour la valeur dans le store
-      this.updateTriPrixAction(this.triPrix)
-    },
-
-    ...mapActions(useMangaStore, { updateSelectedTitleAction: 'updateSelectedTitle' }),
-    exportSelectedTitle() {
-      this.updateSelectedTitleAction(this.selectedTitle)
-    },
-
-  },
-  computed: {
-    ...mapState(useMangaStore, ['mangaList', 'getfilteredList']),
-    uniqueMarques() {
-      const uniqueMarquesSet = new Set()
-
-      this.mangaList.forEach((manga) => {
-        uniqueMarquesSet.add(manga.style)
-      })
-
-      return Array.from(uniqueMarquesSet)
-    }
-  }
+function exportSelectedMarque(){
+  useMangaStore.updateSelectedMarque(); 
 }
+
+function exportTrieAlpha(){
+  useMangaStore.updateTrieAlpha(triAlpha); 
+}
+
+function exportTriePrix(){
+  useMangaStore.updateTriPrix(triPrix); 
+}
+  
+function exportSelectedTitle() {
+  useMangaStore.updateSelectedTitle()
+}
+
+const data = computed(()=>{
+  const uniqueMarquesSet = new Set()
+  useMangaStore.mangaList.forEach((manga)=>{
+    uniqueMarquesSet.add(manga.style)
+  })
+  return Array.from(uniqueMarquesSet)
+})
+
 </script>
 
 <template>
@@ -67,26 +46,23 @@ export default {
     <button @click="exportTrieAlpha">Alphabétique</button>
     <h4>Style</h4>
     <div class="custom-select">
-    <select
-      class="card selectOption"
-      style="width: 18rem"
-      v-model="selectedMarque"
-      @change="exportSelectedMarque"
-    >
-      <option value="all">All</option>
-      <option v-for="(item, index) in uniqueMarques" :key="index" :value="item">{{ item }}</option>
-    </select>
-</div>
+      <select class="card selectOption" style="width: 18rem" v-model="selectedMarque" @change="exportSelectedMarque">
+        <option value="all">All</option>
+        <option v-for="(item, index) in uniqueMarques" :key="index" :value="item">
+          {{ item }}
+        </option>
+      </select>
+    </div>
     <h4>Titre</h4>
-<div class="custom-select">
-    <select class="card selectOption" style="width: 18rem" v-model="selectedTitle" @change="exportSelectedTitle">
-      <option value="all">All</option>
-      <option v-for="(manga, index) in getfilteredList" :key="index" :value="manga">
-        {{ manga.titre }}
-      </option>
-    </select>
-</div>
-    <PanierArticles/>
+    <div class="custom-select">
+      <select class="card selectOption" style="width: 18rem" v-model="selectedTitle" @change="exportSelectedTitle">
+        <option value="all">All</option>
+        <option v-for="(manga, index) in getfilteredList" :key="index" :value="manga">
+          {{ manga.titre }}
+        </option>
+      </select>
+    </div>
+    <PanierArticles />
   </div>
 </template>
 
@@ -112,18 +88,21 @@ select {
   font-size: 16px;
   border: 1px solid #ccc;
   border-radius: 5px;
-  appearance: none; /* Style par défaut de la flèche de la liste déroulante */
+  appearance: none;
+  /* Style par défaut de la flèche de la liste déroulante */
   -webkit-appearance: none;
   -moz-appearance: none;
   cursor: pointer;
 }
 
 .custom-select select:focus {
-  outline: none; /* Supprime la bordure focus par défaut sur certains navigateurs */
+  outline: none;
+  /* Supprime la bordure focus par défaut sur certains navigateurs */
 }
 
 .custom-select::after {
-  content: '\25BC'; /* Flèche vers le bas */
+  content: '\25BC';
+  /* Flèche vers le bas */
   position: absolute;
   top: 50%;
   right: 15px;
