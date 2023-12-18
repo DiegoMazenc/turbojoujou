36007573c93fe11c55b0e21daa280ccc97485445
Diégo Mazenc <contact@diego-mazenc.com>
2023-12-18 11:01:11 +0100

    mise en place de navigation des formulaires admin

--- src/components/compoAdmin/AdminPage.vue
+++ src/components/compoAdmin/AdminPage.vue
@@ -1,42 +1,48 @@
 <script setup>
+import { ref } from 'vue'
 import FormAddJoujou from './FormAddJoujou.vue'
 import FormAddJV from './FormAddJV.vue'
 import FormAddManga from './FormAddManga.vue'
 
-let  currentForm = 'FormAddJoujou'
-
+const isFormAddJoujouVisible = ref(true)
+const isFormAddJVVisible = ref(false)
+const isFormAddMangaVisible = ref(false)
 
 const showForm = (formName) => {
-    currentForm = formName
+    isFormAddJoujouVisible.value = formName === 'FormAddJoujou'
+    isFormAddJVVisible.value = formName === 'FormAddJV'
+    isFormAddMangaVisible.value = formName === 'FormAddManga'
 }
 </script>
 
 <template>
     <div class="ListArticles">
-
+        <!-- Contenu de la liste d'articles -->
     </div>
-  <div class="AddForm">
-    <div class="navForm">
-      <ul>
-        <li>
-          <button class="btn addFig" @click="showForm('FormAddJoujou')">Ajouter une Figurine</button>
-        </li>
-        <li>
-          <button class="btn addMan" @click="showForm('FormAddManga')">Ajouter un Manga</button>
-        </li>
-        <li>
-          <button class="btn addJV" @click="showForm('FormAddJV')">Ajouter un Jeu Video</button>
-        </li>
-      </ul>
-    </div>
+    <div class="AddForm">
+        <div class="navForm">
+            <ul>
+                <li>
+                    <button class="btn addFig" @click="showForm('FormAddJoujou')">Ajouter une Figurine</button>
+                </li>
+                <li>
+                    <button class="btn addMan" @click="showForm('FormAddManga')">Ajouter un Manga</button>
+                </li>
+                <li>
+                    <button class="btn addJV" @click="showForm('FormAddJV')">Ajouter un Jeu Video</button>
+                </li>
+            </ul>
+        </div>
 
-    <FormAddJoujou v-show="currentForm === 'FormAddJoujou'" id="FormAddJoujou" />
-    <FormAddJV v-show="currentForm === 'FormAddJV'" id="FormAddJV" />
-    <FormAddManga v-show="currentForm === 'FormAddManga'" id="FormAddManga" />
- 
-</div>
+        <FormAddJoujou :class="{ 'hidden': !isFormAddJoujouVisible }"/>
+        <FormAddJV :class="{ 'hidden': !isFormAddJVVisible }"/>
+        <FormAddManga :class="{ 'hidden': !isFormAddMangaVisible }"
+        />
+    </div>
 </template>
 
 <style>
-
+.hidden {
+    display: none;
+}
 </style>
