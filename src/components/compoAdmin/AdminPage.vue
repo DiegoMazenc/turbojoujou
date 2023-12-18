495f0b7a02d32be0809e77b4fe84a3c4d0c2e46b
Diégo Mazenc <contact@diego-mazenc.com>
2023-12-18 11:17:53 +0100

    stylisation de la nav admin

--- src/components/compoAdmin/AdminPage.vue
+++ src/components/compoAdmin/AdminPage.vue
@@ -9,40 +9,97 @@ const isFormAddJVVisible = ref(false)
 const isFormAddMangaVisible = ref(false)
 
 const showForm = (formName) => {
-    isFormAddJoujouVisible.value = formName === 'FormAddJoujou'
-    isFormAddJVVisible.value = formName === 'FormAddJV'
-    isFormAddMangaVisible.value = formName === 'FormAddManga'
+  isFormAddJoujouVisible.value = formName === 'FormAddJoujou'
+  isFormAddJVVisible.value = formName === 'FormAddJV'
+  isFormAddMangaVisible.value = formName === 'FormAddManga'
 }
 </script>
 
 <template>
-    <div class="ListArticles">
-        <!-- Contenu de la liste d'articles -->
+  <div class="ListArticles">
+    <!-- Contenu de la liste d'articles -->
+  </div>
+  <div class="AddForm">
+    <div class="navForm">
+      <ul>
+        <li>
+          <button
+            class="btnNav"
+            :class="{ selectBtn: isFormAddJoujouVisible }"
+            @click="showForm('FormAddJoujou')"
+          >
+            Ajouter une Figurine
+          </button>
+        </li>
+        <li>
+          <button
+            class="btnNav"
+            :class="{ selectBtn: isFormAddMangaVisible }"
+            @click="showForm('FormAddManga')"
+          >
+            Ajouter un Manga
+          </button>
+        </li>
+        <li>
+          <button
+            class="btnNav"
+            :class="{ selectBtn: isFormAddJVVisible }"
+            @click="showForm('FormAddJV')"
+          >
+            Ajouter un Jeu Video
+          </button>
+        </li>
+      </ul>
+      <hr />
     </div>
-    <div class="AddForm">
-        <div class="navForm">
-            <ul>
-                <li>
-                    <button class="btn addFig" @click="showForm('FormAddJoujou')">Ajouter une Figurine</button>
-                </li>
-                <li>
-                    <button class="btn addMan" @click="showForm('FormAddManga')">Ajouter un Manga</button>
-                </li>
-                <li>
-                    <button class="btn addJV" @click="showForm('FormAddJV')">Ajouter un Jeu Video</button>
-                </li>
-            </ul>
-        </div>
 
-        <FormAddJoujou :class="{ 'hidden': !isFormAddJoujouVisible }"/>
-        <FormAddJV :class="{ 'hidden': !isFormAddJVVisible }"/>
-        <FormAddManga :class="{ 'hidden': !isFormAddMangaVisible }"
-        />
-    </div>
+    <FormAddJoujou :class="{ hidden: !isFormAddJoujouVisible }" />
+    <FormAddJV :class="{ hidden: !isFormAddJVVisible }" />
+    <FormAddManga :class="{ hidden: !isFormAddMangaVisible }" />
+  </div>
 </template>
 
 <style>
 .hidden {
-    display: none;
+  display: none;
+}
+
+.navForm ul {
+  display: flex;
+  list-style: none;
+  justify-content: space-evenly;
+  margin-bottom: 0;
+}
+
+.navForm hr {
+  position: relative;
+  opacity: 1;
+  margin: 0;
+  z-index: 0;
+  border-color: gray;
+}
+
+.btnNav {
+  position: relative;
+  z-index: 1;
+  background-color: rgba(255, 255, 255, 0);
+  border: none;
+  padding: 10px;
+  margin: 0 0 -1px 0;
+  font-weight: 600;
+  color: gray;
+}
+
+.btnNav:hover {
+  color: rgb(114, 114, 240);
+}
+
+.selectBtn {
+  border-top: 1px gray solid;
+  border-left: 1px gray solid;
+  border-right: 1px gray solid;
+  background-color: rgb(255, 255, 255);
+  border-radius: 10px 10px 0 0;
+  color: rgb(114, 114, 240);
 }
 </style>
